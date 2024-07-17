d707d95ba0ebeff3a36b847e42ae00524579804e
Dax Davis <daxantz@gmail.com>
2024-07-16 22:15:54 -0400

    set state to track input values

--- src/components/EducationDetailsBox.jsx
+++ src/components/EducationDetailsBox.jsx
@@ -1,5 +1,54 @@
 import EducationForm from "./EducationForm"
+import { useState } from "react"
+import { v4 } from "uuid"
 export default function EducationDetailsBox(){
+
+    let [inputValues, setInputValues] = useState({
+        school: '',
+        degree: '',
+        startDate: '',
+        endDate: '',
+        location: ''
+    })
+
+    const handleChange = (input, value) =>{
+        setInputValues(currValues=>({
+            ...currValues,
+            [input]:value
+    }))
+    }
+
+    const [data, setData] = useState([])
+
+    const addItem = (inputValues) =>{
+        
+        setData(currData =>{
+            
+            return [...currData, {id: v4(),
+                school: inputValues.school,
+                degree: inputValues.degree,
+                startDate: inputValues.startDate,
+                endDate: inputValues.endDate,
+                location: inputValues.location,
+                
+            }]
+
+        })
+        setInputValues({
+            school: '',
+            degree: '',
+            startDate: '',
+            endDate: '',
+            location: '',
+            
+        })
+    }
+
+    const deleteItem = (id) =>{
+        setData(currData => currData.filter(dataItem => dataItem.id !== id))
+        
+    }
+    let [isShowing, setIsShowing] = useState(false);
     return(
         <div>
             <h2>Education</h2>
