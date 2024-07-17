57b99f1b403f9169287de8ea736caa7ae0dd8d00
Dax Davis <daxantz@gmail.com>
2024-07-16 22:01:51 -0400

    made education form into own component

--- src/components/EducationDetailsBox.jsx
+++ src/components/EducationDetailsBox.jsx
@@ -1,3 +1,4 @@
+import EducationForm from "./EducationForm"
 export default function EducationDetailsBox(){
     return(
         <div>
@@ -7,33 +8,7 @@ export default function EducationDetailsBox(){
                 <img src="" alt="EYE" />
             </div>
 
-            <form action="">
-                    <div className="input-group">
-                        <label htmlFor="school">School</label>
-                        <input type="text" id="school" name="school"/>
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="Degree">Degree</label>
-                        <input type="text" id="Degree" name="degree"/>
-                    </div>
-                    <div className="date-group">
-                        <label htmlFor="startDate">Start Date</label>
-                        <input type="date" id="startDate" name="startDate"/>
-                    </div>
-                    <div className="date-group">
-                        <label htmlFor="endDate">End Date</label>
-                        <input type="date" id="End Date" name="endDate"/>
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="location">Location</label>
-                        <input type="text" id="location" name="location"/>
-                    </div>
-                    <div className="buttons">
-                        <button>Delete</button>
-                        <button>Save</button>
-                        <button>Cancel</button>
-                    </div>
-            </form>
+            <EducationForm data={inputValues} handleChange={handleChange} addItem={addItem} isShowing={isShowing} setIsShowing={setIsShowing} />
 
             <div>
                 <button>Add Education Item</button>
