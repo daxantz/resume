8ca3b1cd150c6d1b67810048481b6b979d8c3b2c
Dax Davis <daxantz@gmail.com>
2024-07-16 21:44:41 -0400

    created component to hold all detail sections

---
+++ src/components/Forms.jsx
@@ -0,0 +1,15 @@
+import PersonalDetailsBox from "./PersonalDetailsBox"
+import EducationDetailsBox from "./EducationDetailsBox"
+import ExperienceDetailsBox from "./ExperienceDetailsBox"
+export default function Forms(){
+    return (
+        <div>
+            // personal details box
+            <PersonalDetailsBox/>
+            //education details box
+            <EducationDetailsBox/>
+            //experiecne details box
+            <ExperienceDetailsBox/>
+        </div>
+    )
+}
