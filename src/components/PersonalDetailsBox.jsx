4a7ac0f8ef34da8080e37c70a6fab44c7943fcda
Dax Davis <daxantz@gmail.com>
2024-07-22 14:17:02 -0400

    tweaked variable names

--- src/components/PersonalDetailsBox.jsx
+++ src/components/PersonalDetailsBox.jsx
@@ -4,24 +4,24 @@ export default function PersonalDetailsBox({data, handleChange}){
             <h2>Personal Details</h2>
             <form action="">
                 <div className="input-group">
-                    <label htmlFor="fullName">Firsts Name</label>
-                    <input type="text" id="firstName" value={data.firstName} onChange={(e)=> handleChange('firstName', e.target.value)}/>
+                    <label htmlFor="fullName">First Name</label>
+                    <input type="text" id="firstName" value={data.personalInputValues.firstName} onChange={(e)=> handleChange('personalInputValues','firstName', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" id="lastName" value={data.lastName} onChange={(e)=> handleChange('lastName', e.target.value)}/>
+                    <input type="text" id="lastName" value={data.personalInputValues.lastName} onChange={(e)=> handleChange('personalInputValues','lastName', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" value={data.email} onChange={(e)=> handleChange('email', e.target.value)}/>
+                    <input type="email" id="email" value={data.personalInputValues.email} onChange={(e)=> handleChange('personalInputValues','email', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="phoneNumber">Phone Number</label>
-                    <input type="text" id="phoneNumber" value={data.phoneNumber} onChange={(e)=> handleChange('phoneNumber', e.target.value)}/>
+                    <input type="text" id="phoneNumber" value={data.personalInputValues.phoneNumber} onChange={(e)=> handleChange('personalInputValues','phoneNumber', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="address">Address</label>
-                    <input type="text" id="address" value={data.address} onChange={(e)=> handleChange('address', e.target.value)}/>
+                    <input type="text" id="address" value={data.personalInputValues.address} onChange={(e)=> handleChange('personalInputValues','address', e.target.value)}/>
                 </div>
             </form>
 
