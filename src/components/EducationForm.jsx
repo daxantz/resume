79c801dbf47724b7ddb4da75f330f7a2215e369b
Dax Davis <daxantz@gmail.com>
2024-07-22 14:19:31 -0400

    added and cahnged some prop names

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,4 +1,4 @@
-export default function EducationForm({data, handleChange, addItem, updateData, isEditing,setIsEditing, isShowing, setIsShowing, deleteItem}){
+export default function EducationForm({data, handleChange, handleEditingChange, addItem, updateData, isEditing,setIsEditing, isShowing, setIsShowing, deleteItem}){
     function handleFunctions(){
 
         if(isShowing){
@@ -41,23 +41,23 @@ export default function EducationForm({data, handleChange, addItem, updateData, 
         <form action="">
                     <div className="input-group">
                         <label htmlFor="school">School</label>
-                        <input type="text" id="school" name="school" value={data.school} onChange={(e)=>handleChange('school',e.target.value)}/>
+                        <input type="text" id="school" name="school" value={data.school} onChange={(e)=> isShowing ? handleChange('educationInputValues','school',e.target.value): handleEditingChange('school', e.target.value)}/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="Degree">Degree</label>
-                        <input type="text" id="Degree" name="degree" value={data.degree} onChange={(e)=>handleChange('degree',e.target.value)}/>
+                        <input type="text" id="Degree" name="degree" value={data.degree} onChange={(e)=> isShowing ? handleChange('educationInputValues','degree',e.target.value): handleEditingChange('degree', e.target.value)}/>
                     </div>
                     <div className="date-group">
                         <label htmlFor="startDate">Start Date</label>
-                        <input type="date" id="startDate" name="startDate" value={data.startDate} onChange={(e)=>handleChange('startDate',e.target.value)}/>
+                        <input type="date" id="startDate" name="startDate" value={data.startDate} onChange={(e)=> isShowing ? handleChange('educationInputValues','startDate',e.target.value): handleEditingChange('startDate', e.target.value)}/>
                     </div>
                     <div className="date-group">
                         <label htmlFor="endDate">End Date</label>
-                        <input type="date" id="End Date" name="endDate" value={data.endDate} onChange={(e)=>handleChange('endDate',e.target.value)}/>
+                        <input type="date" id="End Date" name="endDate" value={data.endDate} onChange={(e)=> isShowing ? handleChange('educationInputValues','endDate',e.target.value): handleEditingChange('endDate', e.target.value)}/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="location">Location</label>
-                        <input type="text" id="location" name="location" value={data.location} onChange={(e)=>handleChange('location',e.target.value)}/>
+                        <input type="text" id="location" name="location" value={data.location} onChange={(e)=> isShowing ? handleChange('educationInputValues','location',e.target.value): handleEditingChange('location', e.target.value)}/>
                     </div>
                     <div className="buttons">
                         <button onClick={(e)=>{
--- src/components/ExperienceForm.jsx
+++ src/components/ExperienceForm.jsx
@@ -1,4 +1,4 @@
-export default function ExperienceForm({data, handleChange, addItem, updateData, isEditing,setIsEditing, isShowing, setIsShowing, deleteItem}){
+export default function ExperienceForm({data, handleChange, handleEditingChange, addItem, updateData, isEditing,setIsEditing, isShowing, setIsShowing, deleteItem}){
     function handleFunctions(){
 
         if(isShowing){
@@ -35,27 +35,27 @@ export default function ExperienceForm({data, handleChange, addItem, updateData,
         <form action="">
                     <div className="input-group">
                         <label htmlFor="company">Company</label>
-                        <input type="text" id="company" name="company" value={data.company} onChange={(e)=>handleChange('company',e.target.value)}/>
+                        <input type="text" id="company" name="company" value={data.company} onChange={(e)=> isShowing ? handleChange('experienceInputValues','company',e.target.value) : handleEditingChange('company', e.target.value)}/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="position">Position</label>
-                        <input type="text" id="position" name="position" value={data.position} onChange={(e)=>handleChange('position',e.target.value)}/>
+                        <input type="text" id="position" name="position" value={data.position} onChange={(e)=> isShowing ? handleChange('experienceInputValues','position',e.target.value) : handleEditingChange('position', e.target.value)}/>
                     </div>
                     <div className="date-group">
                         <label htmlFor="startDate">Start Date</label>
-                        <input type="date" id="startDate" name="startDate" value={data.startDate} onChange={(e)=>handleChange('startDate',e.target.value)}/>
+                        <input type="date" id="startDate" name="startDate" value={data.startDate} onChange={(e)=> isShowing ? handleChange('experienceInputValues','startDate',e.target.value) : handleEditingChange('startDate', e.target.value)}/>
                     </div>
                     <div className="date-group">
                         <label htmlFor="endDate">End Date</label>
-                        <input type="date" id="End Date" name="endDate" value={data.endDate} onChange={(e)=>handleChange('endDate',e.target.value)} />
+                        <input type="date" id="End Date" name="endDate" value={data.endDate} onChange={(e)=> isShowing ? handleChange('experienceInputValues','endDate',e.target.value) : handleEditingChange('endDate', e.target.value)} />
                     </div>
                     <div className="input-group">
                         <label htmlFor="location">Location</label>
-                        <input type="text" id="location" name="location" value={data.location} onChange={(e)=>handleChange('location',e.target.value)}/>
+                        <input type="text" id="location" name="location" value={data.location} onChange={(e)=> isShowing ? handleChange('experienceInputValues','location',e.target.value) : handleEditingChange('location', e.target.value)}/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="description">Description</label>
-                        <textarea name="description" id="description" cols="30" rows="10" value={data.description} onChange={(e)=>handleChange('description',e.target.value)}></textarea>
+                        <textarea name="description" id="description" cols="30" rows="10" value={data.description} onChange={(e)=> isShowing ? handleChange('experienceInputValues','description',e.target.value) : handleEditingChange('description', e.target.value)}></textarea>
                     </div>
                     <div className="buttons">
                         <button onClick={(e)=>{
