b58fed8a4a0f42e88f8f220f0d75201ca42515ef
Dax Davis <daxantz@gmail.com>
2024-07-21 22:27:19 -0400

    form values are now controlled through state

--- src/components/PersonalDetailsBox.jsx
+++ src/components/PersonalDetailsBox.jsx
@@ -1,27 +1,27 @@
-export default function PersonalDetailsBox(){
+export default function PersonalDetailsBox({data, handleChange}){
     return(
         <div>   
             <h2>Personal Details</h2>
             <form action="">
                 <div className="input-group">
                     <label htmlFor="fullName">Firsts Name</label>
-                    <input type="text" id="firstName"/>
+                    <input type="text" id="firstName" value={data.firstName} onChange={(e)=> handleChange('firstName', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" id="lastName"/>
+                    <input type="text" id="lastName" value={data.lastName} onChange={(e)=> handleChange('lastName', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email"/>
+                    <input type="email" id="email" value={data.email} onChange={(e)=> handleChange('email', e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="phoneNumber">Phone Number</label>
-                    <input type="text" id="phoneNumber"/>
+                    <input type="text" id="phoneNumber" value={data.phoneNumber} onChange={(e)=> handleChange('phoneNumber', e.target.value)}/>
                 </div>
                 <div className="input-group">
-                    <label htmlFor="address">Adress</label>
-                    <input type="text" id="address"/>
+                    <label htmlFor="address">Address</label>
+                    <input type="text" id="address" value={data.address} onChange={(e)=> handleChange('address', e.target.value)}/>
                 </div>
             </form>
 
