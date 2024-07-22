9902a116254afd5ac500844eb8b27c356c669adf
Dax Davis <daxantz@gmail.com>
2024-07-22 14:19:46 -0400

    added additonal props

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,15 +1,15 @@
 import PersonalDetailsBox from "./PersonalDetailsBox"
 import EducationDetailsBox from "./EducationDetailsBox"
 import ExperienceDetailsBox from "./ExperienceDetailsBox"
-export default function Forms({data, handleChange}){
+export default function Forms({data, handleChange, setFormValues, educationData, setEducationData,experienceData, setExperienceData}){
     return (
         <div>
             // personal details box
             <PersonalDetailsBox data={data} handleChange={handleChange}/>
             //education details box
-            <EducationDetailsBox/>
+            <EducationDetailsBox formData={data} handleChange={handleChange} setFormValues={setFormValues} educationData={educationData} setEducationData={setEducationData}/>
             //experiecne details box
-            <ExperienceDetailsBox/>
+            <ExperienceDetailsBox formData={data} handleChange={handleChange} setFormValues={setFormValues} experienceData={experienceData} setExperienceData={setExperienceData}/>
         </div>
     )
 }
