48b2ea7f69cae36e54c35ba4320b75d91e153dec
Dax Davis <daxantz@gmail.com>
2024-07-22 14:15:56 -0400

    renders education and experience items

--- src/components/ResumeBody.jsx
+++ src/components/ResumeBody.jsx
@@ -1,12 +1,21 @@
-import ResumeEducationBox from "./ResumeEducationBox"
-import ResumeExperienceBox from "./ResumeExperienceBox"
-export default function ResumeBody(){
+import ResumeEducationItem from "./ResumeEducationItem"
+import ResumeExperienceItem from "./ResumeExperienceItem"
+import ResumeExperienceBox from "./ResumeExperienceItem"
+export default function ResumeBody({data, educationData, experienceData}){
     return(
         <div>
             {/* resume education box */}
-            <ResumeEducationBox/>
+            <h3>Education</h3>
+            {educationData.map((item) =>(
+                <ResumeEducationItem key={item.id} item={item}/>
+            ))}
+            {/* <ResumeEducationItem educationData={educationData}/> */}
             {/* pro experience box */}
-            <ResumeExperienceBox/>
+            <h3>Experience</h3>
+            {experienceData.map((item) =>(
+                <ResumeExperienceItem key={item.id} item={item}/>
+            ))}
+            {/* <ResumeExperienceBox/> */}
         </div>
     )
 }
